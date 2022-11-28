1800f16e25b39e03da93a81e89a8df60f722c75b
Helenagg <hgonzalezg18@gmail.com>
2022-11-28 20:31:18 +0000

    flask_jwt_extended completed

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { Context } from "../store/appContext";
 import rigoImageUrl from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
@@ -6,21 +6,41 @@ import "../../styles/home.css";
 export const Home = () => {
 	const { store, actions } = useContext(Context);
 
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
+
+	var myHeaders = new Headers();
+		myHeaders.append("Content-Type", "application/json");
+
+	var raw = JSON.stringify({
+		"email": "helena.gonza@hotmail.com",
+		"password": "123456"
+	});
+
+	var requestOptions = {
+		method: 'POST',
+		headers: myHeaders,
+		body: raw,
+		redirect: 'follow'
+	};
+
+	fetch("https://3001-4geeksacade-reactflaskh-ea2apv1hwm8.ws-eu77.gitpod.io/api/login", requestOptions)
+	.then(response => response.json())
+	.then(result => console.log(result))
+	.catch(error => console.log('error', error));
+
 	return (
 		<div className="text-center mt-5">
 			<h1>Hello Rigo!!</h1>
 			<p>
-				<img src={rigoImageUrl} />
+				<label>Email:</label>
+				<input></input>
 			</p>
-			<div className="alert alert-info">
-				{store.message || "Loading message from the backend (make sure your python backend is running)..."}
-			</div>
 			<p>
-				This boilerplate comes with lots of documentation:{" "}
-				<a href="https://start.4geeksacademy.com/starters/react-flask">
-					Read documentation
-				</a>
+				<label>Password:</label>
+				<input></input>
 			</p>
+			<button onClick={"pdte"}>Login</button>
 		</div>
 	);
 };
