356ec968fe9cfac3b3c1abd10aeab3901430baea
Helenagg <hgonzalezg18@gmail.com>
2023-02-14 11:04:24 +0000

    init

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -9,65 +9,13 @@ import { Link, useNavigate } from "react-router-dom";
 
 export const Home = (props) => {
 	const { store, actions } = useContext(Context);
-	const [result, setResult] = useState([]);
-	const [email, setEmail] = useState("");
-	const [password, setPassword] = useState("");
-	const navigate = useNavigate();
-	const [error, setError] = useState("");
-
-	const login = () => {
-
-		var myHeaders = new Headers();
-		myHeaders.append("Content-Type", "application/json");
-
-		var raw = JSON.stringify({
-		"email": email,
-		"password": password
-		});
-
-		var requestOptions = {
-		method: 'POST',
-		headers: myHeaders,
-		body: raw,
-		redirect: 'follow'
-		};
-
-		fetch(`${process.env.BACKEND_URL}/api/login/`, requestOptions)
-		.then(response => response.json())
-		.then(result => {
-			if(result.token) {
-				localStorage.setItem("token", result.token);
-				navigate("/family")
-			}
-			else {
-				setError(result.msg)
-			};
-		})
-		.catch(error => console.log('error', error));
-
-	}
-
+	
 	return (
 		<>
-			<div className="container text-center mt-5 border border-1 border-primary p-3">
-				<div className="container p-3">
-					<h1 className="text-primary">LOGIN</h1>
-					<div className="row justify-content-md-center border border-1 border-light p-5 m-5">
-						<div className="col">
-							<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS3CPFSTpcN-YwAU8Euffs0uXuywMvSYwVpJA&usqp=CAU" style={{height: 250}}/>
-						</div>
-						<div className="col">
-							<input className="m-3" type="text" placeholder="Email" onChange={(event) => setEmail(event.target.value)}/><br/>
-							<input className="m-3" type="password" placeholder="Password" onChange={(event) => setPassword(event.target.value)}/><br/>
-							<button className="btn btn-primary m-2" onClick={login}>Login</button>
-							<Link to="/signup" className="m-2">
-								<button className="btn btn-outline-primary">Signup</button>
-							</Link>
-						</div>
-						{error && <div className="alert alert-danger" role="alert">
-							{error}
-							</div>}
-					</div>
+			<div className="container text-center">
+				<div className="container-fluid">
+					<img src="https://img.pikbest.com/backgrounds/20190730/technology-in-the-concept-of-digital-background-v_1517348jpg!bwr800" style={{height: 400, width:"80%" }}/>
+					<h1 className="">My Profile</h1>
 				</div>
 			</div>
 		</>
