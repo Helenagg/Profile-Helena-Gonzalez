a6fd7e3f708d681d629d0cfbd097bd1eb7f06036
Helenagg <hgonzalezg18@gmail.com>
2023-02-16 11:56:13 +0000

    page init completed

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -1,19 +1,22 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import background from "../../img/fondo_portfolio.jpg"
+import about from "../../img/about.jpg"
 
 export const Navbar = () => {
 	return (
-		<nav className="navbar navbar-light bg-light">
-			<div className="container">
-				<Link to="/">
-					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-				</Link>
-				<div className="ml-auto">
-					<Link to="/demo">
-						<button className="btn btn-primary">Check the Context in action</button>
-					</Link>
+		<>
+			<div className="container-fluid" >
+				<img src={background} className="img-fluid"/>
+				<div className="mt-2">
+				<nav className="nav nav-pills nav-fill">
+					<button className="nav-link active" aria-current="page" href="#">Active</button>
+					<a className="nav-link" href="#">Much longer nav link</a>
+					<a className="nav-link" href="#">Link</a>
+					<a className="nav-link disabled">Disabled</a>
+				</nav>
 				</div>
 			</div>
-		</nav>
+		</>
 	);
 };
