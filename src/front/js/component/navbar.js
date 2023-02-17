342be704ce9fb03678253805e93b52f0e568f8f4
Helenagg <hgonzalezg18@gmail.com>
2023-02-17 08:26:32 +0000

    nav center

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -9,11 +9,11 @@ export const Navbar = () => {
 			<div className="container-fluid" >
 				<img src={background} className="img-fluid"/>
 				<div className="mt-2">
-				<nav className="nav nav-pills nav-fill justify-content-end">
-					<button className="btn color-first m-2" aria-current="page" href="#">Active</button>
-					<button className="btn color-second m-2" aria-current="page" href="#">Active</button>
-					<button className="btn color-first m-2" aria-current="page" href="#">Active</button>
-					<button className="btn color-second m-2" aria-current="page" href="#">Active</button>
+				<nav className="nav nav-pills nav-fill justify-content-center">
+					<a className="btn color-first m-2 px-5" aria-current="page" href="#">CV</a>
+					<a className="btn color-second m-2 px-5" aria-current="page" href="#">Skills</a>
+					<a className="btn color-first m-2 px-5" aria-current="page" href="#">Proyect</a>
+					<a className="btn color-second m-2 px-5" aria-current="page" href="#">Contact</a>
 				</nav>
 				</div>
 			</div>
