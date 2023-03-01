2528697077a08365d7064789379ccab7339f2a37
Helenagg <hgonzalezg18@gmail.com>
2023-03-01 08:21:19 +0000

    env

--- src/front/js/component/footer.js
+++ src/front/js/component/footer.js
@@ -2,9 +2,8 @@ import React, { Component } from "react";
 
 export const Footer = () => (
 	<footer className="footer mt-auto py-3 text-center">
-		<p>
-			Made with <i className="fa fa-heart text-danger" /> by{" "}
-			<a href="http://www.4geeksacademy.com">4Geeks Academy</a>
+		<p className="color-title-first">
+			Image by <a className="text-reset text-decoration-none color-title-second" href="https://www.freepik.com/free-vector/hand-drawn-international-youth-day-instagram-posts-collection-with-photo_15694330.htm#&position=0&from_view=popular">Freepik</a>
 		</p>
 	</footer>
 );
